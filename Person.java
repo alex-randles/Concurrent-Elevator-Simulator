e35ec6691fdf48c1a044c9269defb9c5fcff6b4e
kealant <kealan.thompsonmail.dcu.ie>
2019-02-24 14:27:16 +0000

    added random generation of person attributes

--- Person.java
+++ Person.java
@@ -1,25 +1,47 @@
 import java.util.*;
+import java.lang.Math;
 
 public class Person implements Runnable{
-    int id;
+    int id = 0;
     int arrivalTime; 
     int arrivalFloor;
     int destinationFloor; 
-    boolean hasTrolley;
+    int baggageWeight;
+    int personWeight;
 
-    
-    public Person(int id, int arrivalTime, int arrivalFloor, int destinationFloor, boolean hasTrolley){
+    public Person(){
 		 this.id = id;
-		 this.arrivalTime = arrivalTime;
-		 this.arrivalFloor = arrivalFloor;
-		 this.destinationFloor = destinationFloor; 
-         this.hasTrolley = hasTrolley; 
-    
+		 this.arrivalTime = arrivalTime();
+		 this.arrivalFloor = floor();
+		 this.destinationFloor = floor();
+         this.baggageWeight = luggage();
+         this.personWeight = passengerWeight();
+         id++;
+    }
+
+    public Time arrivalTime(){
+         randomMinute = (Math.random()*((15-0)+1))+0;
+         randomSecond = (Math.random()*((60-0)+1))+0;
+         Time time = new Time(16, randomMinute, randomSecond);
+         return Time;
+
+    }
+
+    public int floor(){
+        return (Math.random()*((10-0)+1))+0;
+    }
+
+    public int luggage(){
+        return (Math.random()*((30-0)+1))+0;
     }
     
     public int getCurrentFloor(Person p1){
          return p1.arrivalFloor; 
 
+    }
+
+    public int passengerWeight(){
+        return (Math.random()*((100-40)+1))+40;
     }
     
     public void run(){
